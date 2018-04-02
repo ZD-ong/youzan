b7ef2509bc38e51a359d2f6a1d3f35ebe26892d8
ZD-ong <384474574@qq.com>
2018-04-02 18:09:18 +0800

    获取mock数据，限制热图请求

---
+++ src/modules/js/api.js
@@ -0,0 +1,21 @@
+let url = {
+    hotLists: '/index/hotLists',
+    banner: '/index/banner'
+}
+
+//开发环境和真实环境的切换
+
+//let host = ''
+let host = 'http://rapapi.org/mockjsdata/24170'
+
+
+
+for(let key in url){
+    if(url.hasOwnProperty(key)){
+        url[key] = host + url[key]
+    }
+}
+
+
+
+export default url
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,15 +1,58 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'css/common.css'
+import './index.css'
+
+
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import axios from 'axios'
+import url from 'js/api.js'
+// import App from './App'
+// import router from './router'
 
-Vue.config.productionTip = false
+import { InfiniteScroll } from 'mint-ui'
+Vue.use(InfiniteScroll)
 
-/* eslint-disable no-new */
+console.log(axios)
+
+// Vue.config.productionTip = false
+
+// /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  data: {
+    lists: null,
+    pageNum: 1,
+    pageSize: 6,
+    loading: false,
+    allLoaded: false
+  },
+  created(){
+    this.getLists()
+  },
+  methods: {
+    getLists(){
+      if(this.allLoaded) return
+      this.loading = true
+      axios.post(url.hotLists, {
+        pageNum: this.pageNum,
+        pageSize: this.pageSize
+      }).then(res => {
+        let curLists = res.data.lists
+        //判断所有数据是否加载完毕
+        if(curLists.length < this.pageSize){
+          this.allLoaded = true
+        }
+        if(this.lists){
+          this.lists = this.lists.concat(curLists)
+        }else {
+          //第一次请求数据
+          this.lists = curLists
+        }
+        this.loading = false
+        this.pageNum++
+      })
+    }
+  }
+
 })
