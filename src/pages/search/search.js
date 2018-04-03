fa5d39a10efcbe2266f2e416be341779c2a71894
ZD-ong <384474574@qq.com>
2018-04-03 21:20:23 +0800

    完成列表页

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,2 +1,43 @@
 import 'css/common.css'
 import './search.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+import qs from 'qs'
+
+import Velocity from 'velocity-animate'
+
+let {keyword, id} = qs.parse(location.search.substr(1))
+
+import mixin from 'js/mixin.js'
+
+new Vue ({
+    el: '.container',
+    data: {
+        searchList: null,
+        isShow: false
+    },
+    created(){
+        this.getSeachList()
+    },
+    methods: {
+        getSeachList(){
+            axios.post(url.searchList,{keyword,id}).then(res => {
+                console.log(res)
+                this.searchList = res.data.lists
+            })
+        },
+        move(){
+            if(document.body,screenTop > 100){
+                this.isShow = true
+            }else{
+                this.isShow = false
+            }
+        },
+        toTop(){
+            Velocity(document.body, 'scroll', {duration: 1000})
+        }
+    },
+    mixins: [mixin]
+})
